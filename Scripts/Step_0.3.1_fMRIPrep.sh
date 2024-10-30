b7cce2c491283870f7edd0fd588e958af73a6a8f
ss <13998040+zhushanshan0717@user.noreply.gitee.com>
2024-10-30 13:18:19 +0800

    added code comments

--- Scripts/Step_0.3.1_fMRIPrep.sh
+++ Scripts/Step_0.3.1_fMRIPrep.sh
@@ -1,10 +1,17 @@
-#User inputs:
+# 定义路径
+# 原始bids数据的目录
 bids_root_dir=/home/ss/ds001734_demo/BIDS
+# 输出结果目录
 bids_root_out=/home/ss/ds001734_demo/BIDS
+# 工作目录
 bids_root_dir_output_wd4singularity=/home/ss/ds001734_demo/BIDS/BIDS_WD
 mkdir $bids_root_dir_output_wd4singularity
+# 接受命令行传入的被试编号
 subj=$1
+# 定义线程数
 nthreads=12
+
+# 创建输出和工作目录
 if [ ! -d $bids_root_out/derivatives ]; then
    mkdir $bids_root_out/derivatives
 fi
@@ -12,12 +19,12 @@ if [ ! -d $bids_root_dir_output_wd4singularity/derivatives ]; then
   mkdir $bids_root_dir_output_wd4singularity/derivatives
 fi
 
-#Run fmriprep
+#运行 fmriprep
 echo ""
 echo "Running fmriprep on participant: sub-$subj"
 echo ""
 
-#Make fmriprep directory and participant directory in derivatives folder
+# 创建fmriprep相关目录
 if [ ! -d $bids_root_out/derivatives/fmriprep ]; then
     mkdir $bids_root_out/derivatives/fmriprep
 fi
@@ -33,7 +40,7 @@ if [ ! -d $bids_root_dir_output_wd4singularity/derivatives/fmriprep/sub-${subj} 
     mkdir $bids_root_dir_output_wd4singularity/derivatives/fmriprep/sub-${subj}
 fi
 
-#Run fmriprep
+# 运行fmriprep
 docker run --rm -it -v $bids_root_dir_output_wd4singularity/derivatives/fmriprep/sub-${subj}:/wd \
     -v $bids_root_dir:/inputbids \
     -v $bids_root_out/derivatives/fmriprep/sub-${subj}:/output \
